ee0630aa0309f858d32cd8ebfcc946be27964373
Michał Cybulski <michal@cybulski.me>
2017-12-25 02:20:37 +0100

    Improved /user-accounts/current endpoint.

--- src/main/java/me/cybulski/civ5pbemserver/user/UserAccountController.java
+++ src/main/java/me/cybulski/civ5pbemserver/user/UserAccountController.java
@@ -1,21 +1,16 @@
 package me.cybulski.civ5pbemserver.user;
 
+import com.google.common.collect.ImmutableSet;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import me.cybulski.civ5pbemserver.exception.ResourceNotFoundException;
 import me.cybulski.civ5pbemserver.user.dto.CurrentUserOutputDTO;
 import me.cybulski.civ5pbemserver.user.dto.RegisterInputDTO;
 import me.cybulski.civ5pbemserver.user.dto.RegisterOutputDTO;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * @author Michał Cybulski
@@ -26,6 +21,7 @@ import java.util.stream.Collectors;
 class UserAccountController {
 
     private static final String ANONYMOUS_USER = "anonymousUser";
+    private static final String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
 
     private final UserAccountApplicationService userAccountApplicationService;
 
@@ -43,26 +39,16 @@ class UserAccountController {
 
     @RequestMapping(path = "current", method = RequestMethod.GET)
     public CurrentUserOutputDTO currentUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName();
-        Set<String> roles = authentication.getAuthorities().stream()
-                                    .map(Object::toString)
-                                    .collect(Collectors.toSet());
-
-        if (ANONYMOUS_USER.equals(email)) {
-            return CurrentUserOutputDTO.builder()
-                           .email(authentication.getName())
-                           .username(authentication.getName())
-                           .roles(roles)
-                           .build();
-        }
-
-        return userAccountApplicationService.findUserByEmail(email)
-                       .map(userAccount -> CurrentUserOutputDTO.builder()
-                                                   .email(userAccount.getEmail())
-                                                   .username(userAccount.getUsername())
-                                                   .roles(roles)
-                                                   .build())
-                       .orElseThrow(ResourceNotFoundException::new);
+        return userAccountApplicationService.getCurrentUserAccount()
+                .map(userAccount -> CurrentUserOutputDTO.builder()
+                        .email(userAccount.getEmail())
+                        .username(userAccount.getUsername())
+                        .roles(userAccount.getRoles())
+                        .build())
+                .orElseGet(() -> CurrentUserOutputDTO.builder()
+                        .email(ANONYMOUS_USER)
+                        .username(ANONYMOUS_USER)
+                        .roles(ImmutableSet.of(ROLE_ANONYMOUS))
+                        .build());
     }
 }
